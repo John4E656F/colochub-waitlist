c809e3064a15e4c717879b79526ff8181f9eb49e
John4E656F <jm49andres@gmail.com>
2024-03-19 12:36:33 +0100

    Building Waitlist with responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { useState } from 'react';
+import Image from 'next/image';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { joinWaitlistFormResolver, type JoinWaitlistFormInputs } from './joinWaitlistFormResolver';
+import waitlistImage from '@/assets/waitlist.svg';
 
 export default function Home() {
   const [message, setMessage] = useState<string | null>(null);
@@ -41,12 +43,24 @@ export default function Home() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('email')} type='email' placeholder='Enter your email' required className='font-black text-black' />
-        <button type='submit'>Join Waitlist</button>
-      </form>
-      {message && <p>{message}</p>}
-    </>
+    <div className='flex flex-col lg:flex-row justify-center items-center h-screen text-white font-white'>
+      <div className='flex flex-col items-center max-w-lg gap-5 '>
+        <div className='flex flex-col items-center gap-1'>
+          <h2 className='text-brand-primary text-3xl lg:text-4xl font-medium'>ColocHub</h2>
+          <h1 className='text-4xl text-center lg:text-5xl font-medium'>Join Our Exclusive Waitlist</h1>
+        </div>
+        <p className='text-center text-2xl'>Be the first to know when we launch and gain early access to our groundbreaking platform.</p>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex items-center'>
+          <input {...register('email')} type='email' placeholder='Enter your email' required className='text-black p-2 rounded-l-lg ' />
+          <button type='submit' className='p-2 bg-blue-500 text-white font-bold rounded-r-lg'>
+            Join Waitlist
+          </button>
+        </form>
+        {message && <p className='text-center mt-4'>{message}</p>}
+      </div>
+      <div className='max-w-lg'>
+        <Image src={waitlistImage} width={500} height={500} alt='Picture of the author' />
+      </div>
+    </div>
   );
 }
