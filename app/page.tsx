9a50512939aeb25b57d03c7d0a31ed4786053b6f
John4E656F <jm49andres@gmail.com>
2024-03-19 13:13:28 +0100

    Update Logo in waitlist

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { joinWaitlistFormResolver, type JoinWaitlistFormInputs } from './joinWaitlistFormResolver';
 import waitlistImage from '@/assets/waitlist.svg';
+import logo from '@/assets/logo.svg';
 
 export default function Home() {
   const [message, setMessage] = useState<string | null>(null);
@@ -46,13 +47,13 @@ export default function Home() {
     <div className='flex flex-col lg:flex-row justify-center items-center h-screen text-white font-white '>
       <div className='flex flex-col items-center max-w-lg gap-5 '>
         <div className='flex flex-col items-center gap-1'>
-          <h2 className='text-brand-primary text-3xl lg:text-4xl font-medium'>ColocHub</h2>
+          <Image src={logo} alt='Picture of the author' className='w-auto max-w-sm h-auto max-h-lg object-fill ' />
           <h1 className='text-4xl text-center lg:text-5xl font-medium'>Join Our Exclusive Waitlist</h1>
         </div>
         <p className='text-center text-2xl'>Be the first to know when we launch and gain early access to our groundbreaking platform.</p>
         <form onSubmit={handleSubmit(onSubmit)} className='flex items-center'>
           <input {...register('email')} type='email' placeholder='Enter your email' required className='text-black p-2 rounded-l-lg ' />
-          <button type='submit' className='p-2 bg-blue-500 text-white font-bold rounded-r-lg'>
+          <button type='submit' className='p-2 bg-blue-500 hover:bg-blue-300 text-white font-bold rounded-r-lg'>
             Join Waitlist
           </button>
         </form>
